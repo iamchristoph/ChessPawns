c86d8cb07f9c7318a7da3bb5dc534d86b416fb57
iamchristoph <iamchristoph@yahoo.com>
2015-09-21 14:18:59 -0600

    From uvschess

---
+++ uvschess/StudentAI/StudentAI.cs
@@ -0,0 +1,153 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UvsChess;
+
+namespace StudentAI
+{
+    public class StudentAI : IChessAI
+    {
+        #region IChessAI Members that are implemented by the Student
+
+        /// <summary>
+        /// The name of your AI
+        /// </summary>
+        public string Name
+        {
+#if DEBUG
+            get { return "StudentAI (Debug)"; }
+#else
+            get { return "StudentAI"; }
+#endif
+        }
+
+        /// <summary>
+        /// Evaluates the chess board and decided which move to make. This is the main method of the AI.
+        /// The framework will call this method when it's your turn.
+        /// </summary>
+        /// <param name="board">Current chess board</param>
+        /// <param name="yourColor">Your color</param>
+        /// <returns> Returns the best chess move the player has for the given chess board</returns>
+        public ChessMove GetNextMove(ChessBoard board, ChessColor myColor)
+        {
+            ChessMove move = GetRndMove(board, myColor);
+            return move;
+        }
+
+        /// <summary>
+        /// Calls GetAllMoves and returns a random move from the array.
+        /// </summary>
+        /// <param name="board"></param>
+        /// <param name="myColor"></param>
+        /// <returns> Returns a random move </returns>
+        public ChessMove GetRndMove(ChessBoard board, ChessColor myColor)
+        {
+            List<ChessMove> moves = GetAllMoves(board, myColor);
+            Random random = new Random();
+            int randInt = random.Next(moves.Count);
+            return moves[randInt];
+        }
+
+        /// <summary>
+        /// Calls GetMove on each piece in the board and adds it to a list
+        /// </summary>
+        /// <param name="board"></param>
+        /// <param name="myColor"></param>
+        /// <returns>list of possible Moves</returns>
+        public List<ChessMove> GetAllMoves(ChessBoard board, ChessColor myColor)
+        {
+            List<ChessMove> moves = new List<ChessMove>();
+            for (int i = 0; i < 8; i++ )
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    ChessLocation location = new ChessLocation(i, j);
+                    moves.Add(GetMove(board, location, myColor));
+                }
+            }
+            return moves;
+        }
+
+        public ChessMove GetMove(ChessBoard board, ChessLocation location, ChessColor myColor)
+        {
+            ChessPiece myPiece = board[location];
+            ChessMove move = new ChessMove(location, /*to*/location);
+            return move;
+        }
+
+        /// <summary>
+        /// Validates a move. The framework uses this to validate the opponents move.
+        /// </summary>
+        /// <param name="boardBeforeMove">The board as it currently is _before_ the move.</param>
+        /// <param name="moveToCheck">This is the move that needs to be checked to see if it's valid.</param>
+        /// <param name="colorOfPlayerMoving">This is the color of the player who's making the move.</param>
+        /// <returns>Returns true if the move was valid</returns>
+        public bool IsValidMove(ChessBoard boardBeforeMove, ChessMove moveToCheck, ChessColor colorOfPlayerMoving)
+        {
+            throw (new NotImplementedException());
+        }
+
+        #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #region IChessAI Members that should be implemented as automatic properties and should NEVER be touched by students.
+        /// <summary>
+        /// This will return false when the framework starts running your AI. When the AI's time has run out,
+        /// then this method will return true. Once this method returns true, your AI should return a 
+        /// move immediately.
+        /// 
+        /// You should NEVER EVER set this property!
+        /// This property should be defined as an Automatic Property.
+        /// This property SHOULD NOT CONTAIN ANY CODE!!!
+        /// </summary>
+        public AIIsMyTurnOverCallback IsMyTurnOver { get; set; }
+
+        /// <summary>
+        /// Call this method to print out debug information. The framework subscribes to this event
+        /// and will provide a log window for your debug messages.
+        /// 
+        /// You should NEVER EVER set this property!
+        /// This property should be defined as an Automatic Property.
+        /// This property SHOULD NOT CONTAIN ANY CODE!!!
+        /// </summary>
+        /// <param name="message"></param>
+        public AILoggerCallback Log { get; set; }
+
+        /// <summary>
+        /// Call this method to catch profiling information. The framework subscribes to this event
+        /// and will print out the profiling stats in your log window.
+        /// 
+        /// You should NEVER EVER set this property!
+        /// This property should be defined as an Automatic Property.
+        /// This property SHOULD NOT CONTAIN ANY CODE!!!
+        /// </summary>
+        /// <param name="key"></param>
+        public AIProfiler Profiler { get; set; }
+
+        /// <summary>
+        /// Call this method to tell the framework what decision print out debug information. The framework subscribes to this event
+        /// and will provide a debug window for your decision tree.
+        /// 
+        /// You should NEVER EVER set this property!
+        /// This property should be defined as an Automatic Property.
+        /// This property SHOULD NOT CONTAIN ANY CODE!!!
+        /// </summary>
+        /// <param name="message"></param>
+        public AISetDecisionTreeCallback SetDecisionTree { get; set; }
+        #endregion
+    }
+}
