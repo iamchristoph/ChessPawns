43df49c8717dfd148134f38cd33054bb1e674394
iamchristoph <iamchristoph@yahoo.com>
2015-09-21 14:55:42 -0600

    IsValidMove implemented
    uses list of all moves to check for valid move

--- uvschess/StudentAI/StudentAI.cs
+++ uvschess/StudentAI/StudentAI.cs
@@ -84,7 +84,8 @@ namespace StudentAI
         /// <returns>Returns true if the move was valid</returns>
         public bool IsValidMove(ChessBoard boardBeforeMove, ChessMove moveToCheck, ChessColor colorOfPlayerMoving)
         {
-            throw (new NotImplementedException());
+            List<ChessMove> validMoves = GetAllMoves(boardBeforeMove, colorOfPlayerMoving);
+            return validMoves.Contains(moveToCheck);
         }
 
         #endregion
